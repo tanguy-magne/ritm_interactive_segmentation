818ac5d2020b12f2bc07867c85bdd49abb707b6a
Tanguy Magne <tanguy.magne@mines-paristech.fr>
2022-03-12 19:57:31 +0100

    better handling of retraining

--- isegm/engine/trainer.py
+++ isegm/engine/trainer.py
@@ -186,8 +186,8 @@ class ISTrainer(object):
             save_checkpoint(self.net, self.cfg.CHECKPOINTS_PATH, prefix=self.task_prefix,
                             epoch=None, multi_gpu=self.cfg.multi_gpu)
 
-            os.system(f"cp {self.cfg.CHECKPOINTS_PATH}/last_checkpoint.pth {self.cfg.BACKUP_CKPT}")
-            with open(f'{self.cfg.BACKUP_CKPT}latest_epochs.txt', 'w') as f:
+            os.system(f"cp {self.cfg.CHECKPOINTS_PATH}/last_checkpoint.pth {self.cfg.BACKUP_CKPT}{self.cfg.exp_name}_last_checkpoint.pth")
+            with open(f'{self.cfg.BACKUP_CKPT}{self.cfg.exp_name}_latest_epochs.txt', 'w') as f:
                 f.write(f'The checkpoint corresponds to epochs {epoch}.\n')
                 f.write(f'The actual learning rate is {self.optim.param_groups[0]["lr"] }.')
 
